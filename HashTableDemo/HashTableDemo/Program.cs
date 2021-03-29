16d5475c7fc575ce06fd684d9447702928c3128e
unknown <nandiniswaraj13@gmail.com>
2021-03-29 13:29:21 +0530

    [Nandini swaraj]Added frequency of words in a sentence

---
+++ HashTableDemo/HashTableDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash table demo"); //() []
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To be or not to be");
+            hash.frequencyOfWords("0");
+        }
+    }
+}
